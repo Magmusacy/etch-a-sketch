147ecb98a6283910a3a744a7478a6e93ac0ba545
Magmusacy <xpfk.098@gmail.com>
2020-02-03 20:55:03 +0100

    Added reset function v1

--- javascript.js
+++ javascript.js
@@ -1,23 +1,33 @@
 const container = document.querySelector('#container');
-const columns = document.createElement('div');
-const rows = document.createElement('div');
+const btn = document.querySelector('#reset')
+let x = 0;
 function grid(x) {
-    //container.cssText = `grid-template-columns: repeat(${x}, 1fr); grid-template-rows: repeat(${x}, 1fr);`;
-  
+    container.style.cssText = `grid-template-columns:repeat(${x}, 1fr)` 
 
-/*for (let j = 0; j < x**2; j++){
-    const columns = document.createElement('div')
-    columns.classList.add('columns')
-    container.appendChild(columns);
-
-}*/
 for (let i = 0; i < x**2; i++) {
     const rows = document.createElement('div');
     rows.classList.add('rows')
     container.appendChild(rows);
     rows.addEventListener('mouseover', (e) => {
-        console.log(e.target.style.background = 'black')
-    } )
+        e.target.classList.add('mouseover')
+        })
 }
 }
+function reset() {
+    const btn = document.querySelector('#reset')
+    btn.addEventListener('click', () => {
+    x = prompt('How many squares per side do u want?','16')
+    if (isNaN(x)) {
+        alert('Choose a number') 
+        return;     // check if the number is a number :O
+    }
+        while (container.firstChild) {         
+            container.removeChild(container.firstChild);
+        }       // Loop that deletes all children from container 
+    grid(x)
+    })
+}
+
+
 grid(16)
+reset()
