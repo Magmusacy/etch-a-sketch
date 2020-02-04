293a334b01d7207a1cb297e644b884a58309ed3d
Magmusacy <xpfk.098@gmail.com>
2020-02-04 21:56:31 +0100

    Made rainbow colors instead of black

--- javascript.js
+++ javascript.js
@@ -1,6 +1,11 @@
 const container = document.querySelector('#container');
 const btn = document.querySelector('#reset')
 let x = 0;
+let color = 0;
+let r = 0;
+let g = 0;
+let b = 0;
+const er = document.querySelector('div')
 function grid(x) {
     
     const height = 640/x-7
@@ -14,9 +19,15 @@ for (let i = 0; i < x**2; i++) {
     rows.setAttribute(`style`, `width:${width}px; height:${height}px;`)
     container.appendChild(rows);
     rows.addEventListener('mouseover', (e) => {
-        e.target.classList.add('mouseover')
+        changeColor()
+        e.target.style.backgroundColor = `rgb(${r},${g},${b})`       
         })
 }
+}
+function changeColor() {
+    r = Math.floor(Math.random()*255+1);
+    g = Math.floor(Math.random()*255+1);
+    b = Math.floor(Math.random()*255+1)
 }
 function reset() {
     const btn = document.querySelector('#reset')
